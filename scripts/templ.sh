08cf5bc4745bcae4ac1c8c9f57ff95fec1e43fd6
IvanKoskov <evanmatthewofficial@gmail.com>
2025-09-07 18:12:05 +0700

    minor changes

---
+++ scripts/templ.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+setup() {
+    local bin_dir="$HOME/.bin"
+    
+    if [ ! -d "$bin_dir" ]; then
+        echo "Bin directory: $bin_dir"
+        mkdir -p "$bin_dir" # -p creates parent directories if needed
+    fi
+}
+
+setup
